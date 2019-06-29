537367c0c8fa0c551b1bf5494680b66b99ecb23a
KimJunHyeok <iasg2004>
2019-06-29 15:09:40 +0900

    first commit

---
+++ instagram_clone01Tests/instagram_clone01Tests.swift
@@ -0,0 +1,34 @@
+//
+//  instagram_clone01Tests.swift
+//  instagram_clone01Tests
+//
+//  Created by HYEOKBOOK on 29/06/2019.
+//  Copyright © 2019 HYEOKBOOK. All rights reserved.
+//
+
+import XCTest
+@testable import instagram_clone01
+
+class instagram_clone01Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}

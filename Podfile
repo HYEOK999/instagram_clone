c0832b2cc872e8f8bc15befaa30892b4f4f3df1e
KimJunHyeok <iasg2004>
2019-06-29 15:34:45 +0900

    storyboard 드로잉 , firebase pod 파일 설치 및 연동

---
+++ Podfile
@@ -0,0 +1,26 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'instagram_clone01' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+pod 'Firebase/Core'
+pod 'Firebase/Auth'
+pod 'SVProgressHUD'
+pod 'Firebase/Database'
+pod 'Firebase/Storage'
+
+  # Pods for instagram_clone01
+
+  target 'instagram_clone01Tests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'instagram_clone01UITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
---
+++ instagram_clone01/FindPwViewController.swift
@@ -0,0 +1,37 @@
+//
+//  FindPwViewController.swift
+//  instagram_clone01
+//
+//  Created by HYEOKBOOK on 29/06/2019.
+//  Copyright © 2019 HYEOKBOOK. All rights reserved.
+//
+
+import UIKit
+
+class FindPwViewController: UIViewController {
+
+    @IBOutlet weak var emailTF: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func findPw(_ sender: Any) {
+    }
+    
+    @IBAction func backBtn(_ sender: Any) {
+        self.navigationController?.popViewController(animated: true)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

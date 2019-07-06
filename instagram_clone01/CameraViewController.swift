ca3503695e946805304461c2978b30bd125b6e88
KimJunHyeok <iasg2004>
2019-07-06 14:50:16 +0900

    CameraVC -> 사진 불러오기, db,storage 추가, clean 설정

--- instagram_clone01/CameraViewController.swift
+++ instagram_clone01/CameraViewController.swift
@@ -7,12 +7,15 @@
 //
 
 import UIKit
+import FirebaseDatabase
+import FirebaseStorage
 
 class CameraViewController: UIViewController {
 
     @IBOutlet weak var photo: UIImageView!
     
     @IBOutlet weak var captionTextView: UITextView!
+    @IBOutlet weak var shareBtn: UIButton!
     
     var selectedImage : UIImage?
     
@@ -26,6 +29,26 @@ class CameraViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        handlePost()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }//키보드 내림
+    
+    func handlePost(){
+        if selectedImage != nil{
+            self.shareBtn.isEnabled = true
+            self.shareBtn.backgroundColor = UIColor.orange
+        }
+        else{
+            self.shareBtn.isEnabled = false
+            self.shareBtn.backgroundColor = UIColor.lightGray
+        }
+    }// share버튼 ON/OFF 활성화
+    
     @objc func handleSelectPhoto(){
         let pickerController = UIImagePickerController()
         pickerController.delegate = self
@@ -33,7 +56,53 @@ class CameraViewController: UIViewController {
     }
 
     @IBAction func postingBtn(_ sender: Any) {
+        if let profileImg = self.selectedImage, let imgData = profileImg.jpegData(compressionQuality: 0.1){
+            let photoIdString = NSUUID().uuidString
+            let storageRef = Storage.storage().reference(forURL: Config.STORAGE_ROOF_REF).child("posts").child(photoIdString)
+            
+            storageRef.putData(imgData, metadata: nil) { (metaData, error) in
+                if error != nil{
+                    print(error?.localizedDescription)
+                    return
+                }
+                else{
+                    storageRef.downloadURL(completion: { (url, error) in
+                        self.sendDataToDB(photoURL: "\(url)")
+                    })
+                }
+            }
+        }
+    }
+    
+    func sendDataToDB(photoURL : String){
+        let ref = Database.database().reference()
+        let postsReference = ref.child("posts")
+        let newPostId = postsReference.childByAutoId().key
+        let newPostRef = postsReference.child(newPostId!)
+//        newPostRef.setValue([
+//            "photoURL" : photoURL,
+//            "caption" : captionTextView.text!
+//            ])
+        newPostRef.setValue([
+                "photoURL" : photoURL,
+                "caption" : captionTextView.text!
+        ]) { (err, ref) in
+            if err != nil{
+                return
+            }
+            else{
+                self.clean()
+                print("complete")
+            }
+        }
         
+    }
+    
+    
+    func clean(){
+        self.captionTextView.text = ""
+        self.photo.image = UIImage(named: "placeholder-photo")
+        self.selectedImage = nil
     }
 }
 
@@ -41,5 +110,10 @@ extension CameraViewController : UIImagePickerControllerDelegate, UINavigationCo
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         print("didFinishPickingMediaWithInfo")
         
+        if let pickingImg = info[.originalImage] as? UIImage {
+            selectedImage = pickingImg
+            photo.image = pickingImg
+        }
+        dismiss(animated: true, completion: nil)
     }
 }

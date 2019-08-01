8af901df8e7bbe45b1011d1b374989733f0f6674
KimJunHyeok <iasg2004>
2019-08-01 20:38:04 +0900

    DiscoverViewController 업데이트
    DiscoverViewController 소스 업대이트,
    PostApi observeTopPosts() 기능 추가

---
+++ instagram_clone01/Cells/DisCoverCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  DisCoverCollectionViewCell.swift
+//  instagram_clone01
+//
+//  Created by HYEOKBOOK on 01/08/2019.
+//  Copyright © 2019 HYEOKBOOK. All rights reserved.
+//
+
+import UIKit
+
+class DisCoverCollectionViewCell: UICollectionViewCell {
+    
+}
